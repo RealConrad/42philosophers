0763db2e61bf93793a6466b55132dc51db52a6c6
cwenz <wenzconrad@gmail.com>
2023-10-02 15:55:20 +0200

    wip

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 12:46:03 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/02 15:19:44 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/02 15:40:19 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int	main(int argc, char **argv)
 		return (ERROR);
 
 	monitor_philosophers(&simulation_context);
-	detach_threads(&simulation_context);
+	join_threads(&simulation_context);
 	pthread_mutex_unlock(&simulation_context.print_mutex);
+	printf("exit sim %d ? %s\n", simulation_context.philosphers->index, simulation_context.philosphers->exit_sim == true ? "true":"false");
 	return (SUCCESS);
 }
--- philo/src/utils/utils.c
+++ philo/src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 19:05:02 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/01 19:08:07 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/02 15:54:47 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,35 @@ void	detach_threads(t_simulation_state *simulation_context)
 			pthread_detach(temp->thread);
 		if (temp == simulation_context->philosphers)
 			break;
+	}
+}
+
+void	join_threads(t_simulation_state *simulation_context)
+{
+	t_philosopher *temp;
+
+	temp = simulation_context->philosphers;
+	while (true)
+	{
+		// printf("--------------- Philo [%d] ate: %d\n", temp->index, temp->eat_count);
+		temp = temp->next;
+		if (temp->thread)
+			pthread_join(temp->thread, NULL);
+		if (temp == simulation_context->philosphers)
+			return ;
+	}
+}
+
+void	exit_all_threads(t_simulation_state *simulation_context)
+{
+	t_philosopher *temp;
+
+	temp = simulation_context->philosphers;
+	while (true)
+	{
+		temp->exit_sim = true;
+		temp = temp->next;
+		if (temp == simulation_context->philosphers)
+			return ;
 	}
 }

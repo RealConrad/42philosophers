66451146d2f3b60fd5ace6c8fc481d5d28efc4be
cwenz <wenzconrad@gmail.com>
2023-10-06 16:12:55 +0200

    fixed norm errors for print

--- philo/src/print/print.c
+++ philo/src/print/print.c
@@ -6,24 +6,35 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 14:30:58 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/06 15:29:34 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/06 16:12:34 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	print_philosopher_state(t_philosopher *philosopher, t_philosopher_state state)
+void	print_philosopher_state(t_philosopher *philosopher,
+	t_philosopher_state state)
 {
 	pthread_mutex_lock(&philosopher->philo_mutex);
 	if (state == TAKEN_FORK && !philosopher->exit_sim)
-		printf("%lld %d %s\n", get_time_difference(*(philosopher->start_time_ms)), philosopher->index, FORK_TEXT);
+		printf("%lld %d %s\n",
+			get_time_difference(*(philosopher->start_time_ms)),
+			philosopher->index, FORK_TEXT);
 	else if (state == EATING && !philosopher->exit_sim)
-		printf("%lld %d %s\n", get_time_difference(*(philosopher->start_time_ms)), philosopher->index, EATING_TEXT);
+		printf("%lld %d %s\n",
+			get_time_difference(*(philosopher->start_time_ms)),
+			philosopher->index, EATING_TEXT);
 	else if (state == THINKING && !philosopher->exit_sim)
-		printf("%lld %d %s\n", get_time_difference(*(philosopher->start_time_ms)), philosopher->index, THINKING_TEXT);
+		printf("%lld %d %s\n",
+			get_time_difference(*(philosopher->start_time_ms)),
+			philosopher->index, THINKING_TEXT);
 	else if (state == SLEEPING && !philosopher->exit_sim)
-		printf("%lld %d %s\n", get_time_difference(*(philosopher->start_time_ms)), philosopher->index, SLEEP_TEXT);
+		printf("%lld %d %s\n",
+			get_time_difference(*(philosopher->start_time_ms)),
+			philosopher->index, SLEEP_TEXT);
 	else if (state == DEATH && !philosopher->exit_sim)
-		printf("\033[31m%lld %d %s\n\033[0m", get_time_difference(*(philosopher->start_time_ms)), philosopher->index, DEATH_TEXT);
+		printf("\033[31m%lld %d %s\n\033[0m",
+			get_time_difference(*(philosopher->start_time_ms)),
+			philosopher->index, DEATH_TEXT);
 	pthread_mutex_unlock(&philosopher->philo_mutex);
 }

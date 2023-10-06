a0685f9a60650b6e4679ac6be3cdcf9a9bcab739
cwenz <wenzconrad@gmail.com>
2023-10-06 17:02:19 +0200

    added check for 0 philosophers

--- philo/src/utils/input_utils.c
+++ philo/src/utils/input_utils.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 12:53:24 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/06 16:08:28 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/06 17:01:30 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ bool	check_input(int argc, char **argv)
 	int	i;
 
 	i = 0;
+	if (atol(argv[0]) == 0)
+		return (false);
 	while (i < argc)
 	{
 		if (!check_numeric(argv[i]) || atol(argv[i]) > INT_MAX)

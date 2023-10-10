ec20ba7528189bfdc5055d540bc00e23a73bec6d
cwenz <wenzconrad@gmail.com>
2023-10-10 07:48:32 +0200

    fixed memory leak

--- philo_bonus/src/free/free.c
+++ philo_bonus/src/free/free.c
@@ -6,18 +6,18 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 17:30:50 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/10 06:31:39 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/10 07:48:05 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers_bonus.h"
 
-void	kill_and_free(t_simulation_state *simulaiton_context)
+void	kill_and_free(t_simulation_state *simulation_context)
 {
 	t_philosopher	*philosopher;
 	t_philosopher	*next_philosopher;
 
-	philosopher = simulaiton_context->philosphers;
+	philosopher = simulation_context->philosphers;
 
 	// printf("\033[31m\n\n------- KILLING PROCESSES! --------\n\033[0m \n");
 	while (true)
@@ -27,16 +27,18 @@ void	kill_and_free(t_simulation_state *simulaiton_context)
 		sem_close(philosopher->philo_sem);
 		sem_unlink(philosopher->sem_name);
 		free(philosopher->sem_name);
-		free(philosopher);
+		free(philosopher); 
 		
 		philosopher = next_philosopher;
-		if (philosopher == simulaiton_context->philosphers)
+		if (philosopher == simulation_context->philosphers)
 			break ;
 	}
-	sem_close(philosopher->sim_data->forks);
-	sem_close(philosopher->sim_data->print);
-	sem_close(philosopher->sim_data->is_done_eating);
-	sem_close(philosopher->sim_data->exit_program);
+	sem_post(simulation_context->sim_data.print);
+
+	sem_close(simulation_context->sim_data.forks);
+	sem_close(simulation_context->sim_data.print);
+	sem_close(simulation_context->sim_data.is_done_eating);
+	sem_close(simulation_context->sim_data.exit_program);
 	
 	sem_unlink("/forks");
 	sem_unlink("/print");

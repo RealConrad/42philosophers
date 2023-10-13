ee8e08fe280a3ed23c280fe0ac3556b712c1e68b
cwenz <wenzconrad@gmail.com>
2023-10-13 13:34:51 +0200

    added doxygen for print

--- philo/src/print/print.c
+++ philo/src/print/print.c
@@ -6,12 +6,20 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 14:30:58 by cwenz             #+#    #+#             */
-/*   Updated: 2023/10/12 12:35:10 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/10/13 13:34:30 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+/**
+ * @brief rints out the current state of a specified philosopher to the standard output.
+ * 
+ * This function is thread-safe, as it locks the philosopher's mutex before accessing
+ * or modifying the philosopher's state, and unlocks the mutex before returning.
+ * @param philosopher The philosopher whose state is being printed.
+ * @param state The given state to be printed.
+ */
 void	print_philosopher_state(t_philosopher *philosopher,
 	t_philosopher_state state)
 {
